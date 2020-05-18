4ca710c92c2131ab5792e8744ff3fa83a05d4555
sora-uzu <naruto07170717@gmail.com>
2020-05-18 17:41:23 +0900

    [Update]showsize

--- jquery.showsize.js
+++ jquery.showsize.js
@@ -1,10 +1,10 @@
 ;(function($) {
-	$.fn.showsize = function() {
+	$.fn.showsize = function(options) {
 
 		var elements = this;
 
 		elements.each(function() {
-
+			var opts = $.extend({}, $.fn.showsize.defaults, options);
 			$(this).click(function() {
 				var msg = $(this).width() + ' x ' + $(this).height();
 				$(this).wrap('<div style="position:relative;"></div>');
@@ -14,8 +14,8 @@
 							.css('top', '0')
 							.css('background', 'black')
 							.css('color', 'white')
-							.css('font-size', '10px')
-							.css('opacity', '0.9')
+							.css('font-size', opts.size + 'px')
+							.css('opacity', opts.opacity)
 							.css('padding', '2px');
 			    $(this).after(div);
 			});
@@ -23,4 +23,10 @@
 
 		return this;
 	};
+
+	$.fn.showsize.defaults = {
+		size: 10,
+		opacity: 0.9
+	};
+
 })(jQuery);
